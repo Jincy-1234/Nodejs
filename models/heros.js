7a5fa4363e198155980a9142a6651e17498a867a
Jincy-1234 <jincysjohnson@gmail.com>
2018-11-22 12:25:36 +0530

    first commit

---
+++ models/heros.js
@@ -0,0 +1,156 @@
+var express = require('express');
+var JSONData = require('./heros.json');
+var fs = require("fs");
+
+const mysql=require('mysql2'); 
+let Heros= {}
+/*Heros.getAll = function(){
+    return JSONData}
+Heros.saveNew = function(newHeroData){
+    JSONData.push(newHeroData);
+    fs.writeFile('./heros.json', JSONData, function(err) {
+        if(err){
+            console.lgo('ERR.')
+        }   
+    })
+}*/
+
+
+Heros.getAll = function(){
+	return new Promise(function(resolve,reject){ 
+  		const connection = mysql.createConnection({
+ 	 	host: 'localhost',
+ 	 	user: 'root',
+  		database: 'heroes',password:'ccs#1234'
+   
+	});
+
+	let query='select * from Comic where is_valid=1';
+	
+	connection.query(query,function(err,result,fields){
+		if(err){
+			console.log(err);
+			console.log('ERR:: fetching data from database.');
+			reject();
+		}
+		else{
+		//console.log(result);
+		//console.log(fields);
+		resolve(result);
+		}
+
+	});
+	
+	});
+}
+
+Heros.saveNew = function(newHeroData){
+	return new Promise(function(resolve,reject){ 
+  		const connection = mysql.createConnection({
+ 	 	host: 'localhost',
+ 	 	user: 'root',
+  		database: 'heroes',password:'ccs#1234'
+   
+	});
+  		let query=`insert into Comic(Superhero,publisher,alter_ego,first_app,characters,is_valid,update_time) values('${newHeroData.superhero}','${newHeroData.publishr}','${newHeroData.alterego}','${newHeroData.firstapp}','${newHeroData.charactr}',1,'${new Date()}')`;
+	
+	connection.query(query,function(err,result,fields){
+		if(err){
+			console.log(err);
+			console.log('ERR:: fetching data from database.');
+			reject();
+		}
+		else{
+		//console.log(result);
+		//console.log(fields);
+		resolve(result);
+		}
+
+	});
+	
+	});
+    }
+    Heros.deleteAll = function(deleteHeroData){
+	return new Promise(function(resolve,reject){ 
+  		const connection = mysql.createConnection({
+ 	 	host: 'localhost',
+ 	 	user: 'root',
+  		database: 'heroes',password:'ccs#1234'
+   
+	});
+
+	let query=`update Comic set is_valid=0 where id= ${deleteHeroData.id}`;
+	console.log(query);
+	connection.query(query,function(err,result,fields){
+		if(err){
+			console.log(err);
+			console.log('ERR:: fetching data from database.');
+			reject();
+		}
+		else{
+		//console.log(result);
+		//console.log(fields);
+		resolve(result);
+		}
+
+	});
+	
+	});
+}
+
+
+    Heros.ViewAll = function(deleteHeroData){
+	return new Promise(function(resolve,reject){ 
+  		const connection = mysql.createConnection({
+ 	 	host: 'localhost',
+ 	 	user: 'root',
+  		database: 'heroes',password:'ccs#1234'
+   
+	});
+
+	let query=`select * from Comic where id= '${deleteHeroData.id}'`;
+	
+	connection.query(query,function(err,result,fields){
+		if(err){
+			console.log(err);
+			console.log('ERR:: fetching data from database.');
+			reject();
+		}
+		else{
+		//console.log(result);
+		//console.log(fields);
+		resolve(result);
+		}
+
+	});
+	
+	});
+}
+
+Heros.updateAll = function(newHeroData){
+	return new Promise(function(resolve,reject){ 
+  		const connection = mysql.createConnection({
+ 	 	host: 'localhost',
+ 	 	user: 'root',
+  		database: 'heroes',password:'ccs#1234'
+   
+	});
+  		let query=`update Comic set Superhero='${newHeroData.superhero}',publisher='${newHeroData.publishr}',alter_ego='${newHeroData.alterego}',first_app='${newHeroData.firstapp}',characters='${newHeroData.charactr}',is_valid=1,update_time='${new Date()}' where id= '${newHeroData.id}'`;
+	console.log(query);
+	connection.query(query,function(err,result,fields){
+		if(err){
+			console.log(err);
+			console.log('ERR:: fetching data from database.');
+			reject();
+		}
+		else{
+		//console.log(result);
+		//console.log(fields);
+		resolve(result);
+		}
+
+	});
+	
+	});
+    }
+module.exports = Heros;
---
+++ routes/index.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var router = express.Router();
+var Heros=require('../models/heros.js');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title:'HERO HUNTERS'});
+});
+
+/*router.get('/saveData', function(req, res, next) {
+ console.log(req.query);
+ res.send(req.query);
+});*/
+
+router.get('/saveData', function(req, res, next) {
+	Heros.saveNew(req.query)
+ 	.then(function(){
+ 		res.redirect('/getAllHeros')
+	})
+	.catch(console.log('ERR::in resolving the promise'))
+});
+
+
+
+router.get('/deleteData', function(req, res, next) {
+	Heros.deleteAll(req.query)
+ 	.then(function(){
+ 		res.redirect('/getAllHeros')
+	})
+	.catch(console.log('ERR::in resolving the promise'))
+});
+
+
+router.get('/getAllHeros', function(req, res, next) {
+	Heros.getAll()
+	.then(function(retVal){
+ 		res.render('heros',{data:retVal})
+	})
+	.catch(console.log('ERR::in resolving the promise'))
+});
+
+router.get('/viewHeros', function(req, res, next) {
+	Heros.ViewAll(req.query)
+	.then(function(retVal){
+ 		res.render('hero',{data:retVal})
+	})
+	.catch(console.log('ERR::in resolving the promise'))
+});
+
+router.get('/UpdateData', function(req, res, next) {
+
+	Heros.updateAll(req.query)
+	.then(function(retVal){
+ 		res.render('update',{data:retVal})
+ 	
+	})
+	Heros.updateAll(req.query)
+	.then(function(retVal){
+ 		res.redirect('/getAllHeros')
+	})
+
+
+	.catch(console.log('ERR::in resolving the promise'))
+});
+
+module.exports = router;
